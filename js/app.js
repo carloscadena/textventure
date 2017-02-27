24b86f0d5b21fc0dac7cbad18cbc64a5d833c0a8
Cameron Bacon <cameronmbacon@gmail.com>
2017-02-27 15:04:28 -0800

    completed basic functionality of app.js

--- js/app.js
+++ js/app.js
@@ -1,40 +1,46 @@
 'use strict';
 
-var mainQuestions = ['Your big adventure starts here, in your room. What will you do next?'];
+var mainQuestions = ['main question 1.', 'main question 2.', 'main question 3.'];
 
-var altQuestions = ['Bad choice! The fire escape is rusted. You fall one story and sprain your ankle.'];
+var altQuestions = ['alternate question 1 (redirect from question 1).',
+  'alternate question 2 (redirect from question 2).'];
 
-var answers = [
-  ['Open the front door.', 'Crawl out the window.', 'Wait for 5 more minutes.'],];
+var mainAnswers = [
+  ['main answer 1', 'main answer 2', 'main answer 3'],
+  ['main answer 1', 'main answer 2', 'main answer 3'],
+  ['main answer 1', 'main answer 2', 'main answer 3']];
+
+var altAnswers = [
+  ['alt answer 1', 'alt answer 2', 'alt answer 3'],
+  ['alt answer 1', 'alt answer 2', 'alt answer 3']];
 
 var sectionEl = document.getElementById('game-content');
 
-var count = 0;
+var i = 0;
 
-function displayQuestion() {
+var questionLimit = mainQuestions.length;
+
+function displayQuestion(question) {
   var el = document.createElement('p');
   el.setAttribute('id', 'questionEl');
-  el.textContent = mainQuestions[count];
+  el.textContent = question[i];
   sectionEl.appendChild(el);
-  count++;
-
-  displayAnswers();
+  console.log(question[i]);
 };
 
-function displayAnswers() {
+function displayAnswers(answers) {
   var formEl = document.createElement('form');
+  formEl.setAttribute('id', 'answer-form');
 
-  var currentAnswers = [];
-
-  for (var i = 0; i < 3; i++) {
+  for (var j = 0; j < 3; j++) {
     var inputEl = document.createElement('input');
-    inputEl.setAttribute('id', 'answer' + i);
+    inputEl.setAttribute('id', 'answer' + j);
     inputEl.setAttribute('type', 'radio');
     inputEl.setAttribute('name', 'answer');
-    inputEl.setAttribute('value', i + 1);
+    inputEl.setAttribute('value', j);
 
     var labelEl = document.createElement('label');
-    labelEl.textContent = answers[0][i];
+    labelEl.textContent = answers[i][j];
 
     inputEl.appendChild(labelEl);
     formEl.appendChild(inputEl);
@@ -45,27 +51,49 @@ function displayAnswers() {
   submitEl.setAttribute('name', 'submit');
   submitEl.setAttribute('value', 'submit');
 
-  submitEl.addEventListener('submit', handleSubmit);
+  formEl.addEventListener('submit', handleSubmit);
 
   formEl.appendChild(submitEl);
   sectionEl.appendChild(formEl);
 };
 
 function handleSubmit(event) {
-  var answerOne = getElementById('answer0');
-  var answerTwo = getElementById('answer1');
-  var answerThree = getElementById('answer2');
-  var userAnswerId = event.target.getAttribute('id');
+  console.log(event);
+  event.preventDefault();
+  i++;
+  var answerOneEl = document.getElementById('answer0');
+  console.log(answerOneEl.id);
+  var answerTwoEl = document.getElementById('answer1');
+  console.log(answerTwoEl.id);
+  var answerThreeEl = document.getElementById('answer2');
+  console.log(answerThreeEl.id);
+  var userAnswerId = event.target[0].checked;
+  console.log(userAnswerId);
 
-  if (userAnswerId === answerOne.id) {
+  if (event.target[0].checked) {
+    var formEl = document.getElementById('answer-form');
     sectionEl.removeChild(formEl);
-    displayQuestion();
-  } else if (userAnswerId === answerTwo.id) {
-
-  } else if (userAnswerId === answerOne.id) {
-
+    console.log('User stayed on main path.');
+    displayQuestion(mainQuestions);
+    displayAnswers(mainAnswers);
+  } else if (event.target[1].checked) {
+    i--;
+    var formEl = document.getElementById('answer-form');
+    sectionEl.removeChild(formEl);
+    console.log('User switched to alternate path.');
+    displayQuestion(altQuestions);
+    displayAnswers(altAnswers);
+  } else if (event.target[2].checked) {
+    var formEl = document.getElementById('answer-form');
+    sectionEl.removeChild(formEl);
+    console.log('User has died.');
   }
 };
 
 //------------------TEST APPLICATION------------------
-displayQuestion();
+if (i === questionLimit) {
+  console.log('User has finished the text adventure.');
+} else {
+  displayQuestion(mainQuestions);
+  displayAnswers(mainAnswers);
+}
